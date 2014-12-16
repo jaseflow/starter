a1259043fde649209d0968624de81a363741e4a4
Jason Corbett <jason.corbett@wotifgroup.com>
2014-12-16 10:47:05 +1000

    Update

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+    
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        stylus: {
+            compile: {
+                files: {
+                    'public/css/app.css': 'source/styles/app.styl'
+                }
+            }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    useAvailablePort: true,
+                    base: 'public',
+                    hostname: '*',
+                    livereload: true
+                }
+            }
+        },
+
+        autoprefixer: {
+            options: {},
+            no_dest: {
+                src: 'public/css/app.css'
+            }
+        },
+
+        watch: {
+           stylus: {
+               files: ['styles/app.styl'],
+               tasks: ['stylus']
+           },
+           livereload: {
+               options: {
+                   livereload: true
+               },
+               files: [
+                   'public/css/app.css',
+                   'public/index.html'
+               ]
+           }
+        }
+
+    });
+
+
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-stylus'); 
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Register tasks
+    grunt.registerTask('default', ['stylus','autoprefixer']);
+    grunt.registerTask('server', ['connect','watch']);
+}
